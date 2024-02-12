4774e7648236659a3a9f3cfaa3a5b1970f0f98ef
asour <52638772+asour8@users.noreply.github.com>
2024-02-12 10:04:52 -0400

    fix

--- src/index.ts
+++ src/index.ts
@@ -119,7 +119,7 @@ function render(time: number) {
 
 let geckosURL = `${location.protocol}//${location.hostname}`;
 if(location.hostname === 'table.carroted.org') {
-    geckosURL = `http://tabledirect.carroted.org`;
+    geckosURL = `${location.protocol}//tabledirect.carroted.org`;
     console.log('using direct geckos.io server');
 } else {
     console.log('using local geckos.io server');
