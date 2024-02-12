1878ae7678f1460e3c6df129a4cb646f2c488597
asour <52638772+asour8@users.noreply.github.com>
2024-02-12 13:10:54 -0400

    e

--- scripts/server.ts
+++ scripts/server.ts
@@ -8,6 +8,9 @@ var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
 var app = express();
 
+// static the dist/ folder
+app.use(express.static('dist'));
+
 // your express configuration here
 
 var httpServer = http.createServer(app);
@@ -15,3 +18,5 @@ var httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(80);
 httpsServer.listen(443);
+
+console.log('we are Listening to you.');
